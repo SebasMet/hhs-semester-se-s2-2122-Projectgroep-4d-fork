9479543cf6c02dc20795f69f95c4c2722d7ceffa
Jochem Lammertsma <30724588+jochemlam@users.noreply.github.com>
2022-05-19 15:01:37 +0100

    new button added for confirmation of data

--- src/main/java/com/example/greenfuture/DefineTripController.java
+++ src/main/java/com/example/greenfuture/DefineTripController.java
@@ -4,8 +4,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,12 +14,30 @@ import java.util.ResourceBundle;
 public class DefineTripController implements Initializable {
 
     @FXML
+    private Button confirmBtn;
+
+    @FXML
     private ComboBox vehicles;
+
+    @FXML
+    private TextField kms;
 
     @FXML
     private ObservableList<String> vehicleList = FXCollections.observableArrayList(
             "Benzine auto", "Hybride auto", "Diesel auto", "Elektrische auto", "Fiets", "Bus"
     );
+
+    @FXML
+    private void confirmTrip() {
+        // controleer of alles is ingevuld
+        if (kms.getCharacters().isEmpty()) return;
+        if (vehicles.getSelectionModel().isEmpty()) return;
+
+        // maak nieuw variable met correcte data
+        System.out.println("dit moet je niet zien");
+
+        // sla data ergens op
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
