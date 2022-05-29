229f98f3bf2ce197a3e0ae040ae550991d8a1549
Jochem Lammertsma <30724588+jochemlam@users.noreply.github.com>
2022-05-29 23:32:08 +0200

    creates new screen properly now
    but opens it in a new window

--- src/main/java/com/example/greenfuture/controllers/Controller.java
+++ src/main/java/com/example/greenfuture/controllers/Controller.java
@@ -7,16 +7,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public abstract class Controller {
 
     protected void changeScene(ActionEvent event, String filename, String titleName) throws IOException {
 
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("fxml/" + filename + ".fxml"));
+        loader.setLocation(Controller.class.getResource("fxml/" + filename + ".fxml"));
         Scene scene = new Scene(loader.load(), 800, 600);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle(titleName);
@@ -27,12 +29,12 @@ public abstract class Controller {
     protected void changeScene(String filename, String titleName) throws IOException {
 
         ActionEvent event = new ActionEvent();
+        Stage stage = new Stage();
+        URL url = getClass().getResource("/com/example/greenfuture/fxml/" + filename + ".fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        Parent parent = loader.load();
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("fxml/" + filename + ".fxml"));
-
-        Scene scene = new Scene(loader.load(), 800, 600);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(parent, 800, 600);
         stage.setTitle(titleName);
         stage.setScene(scene);
         stage.show();
