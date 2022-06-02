20798c412b16cacb1587d4a007fe4736a00bc3a3
MaikeruB <maikelbazuin@gmail.com>
2022-06-02 11:38:35 +0200

    update
    nieuwe methodes en test
    en een test verbeterd

--- src/main/java/com/example/greenfuture/ScoreBoard.java
+++ src/main/java/com/example/greenfuture/ScoreBoard.java
@@ -1,8 +1,13 @@
 package com.example.greenfuture;
 
-import java.util.ArrayList;
+import javafx.fxml.Initializable;
 
-public class ScoreBoard {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ResourceBundle;
+
+public class ScoreBoard implements Comparable<ScoreBoard>, Initializable {
     private int points;
     private int UserID;
 
@@ -14,16 +19,50 @@ public class ScoreBoard {
 
     }
 
-    public void AddPoints(int points, int userID){
-        int 
+    public int CheckForUserID(int userID){ //checkt of de User al een value heeft in de arraylist scoreboardpoints
+        int User = -1;
         for (int i = 0; i < scoreboardPoints.size(); i++){
-            if ()
+            if (scoreboardPoints.get(i).getUserID() == userID){
+                User = i;
+            }
+        }
+        return User;
+    }
+
+
+    public void AddPoints(int points, int userID){ // voegt punten toe aan de user als de user nog geen punten heeft wordt die aangemaakt
+        int User = CheckForUserID(userID);
+        if (User == -1){
+            scoreboardPoints.add(new ScoreBoard(points, userID));
+        } else{
+            int NewPoints = points + scoreboardPoints.get(User).getPoints();
+            scoreboardPoints.get(User).setPoints(NewPoints);
         }
 
     }
 
+    public void PrintScoreboardPoints(){ // print de arraylist scoreboardPoints
+        for (int i = 0; i < scoreboardPoints.size(); i++){
+            System.out.println(i + ": " + scoreboardPoints.get(i).getUserID() + " Punten: " + scoreboardPoints.get(i).getPoints());
+        }
+    }
+
+    public void SortScoreboardPoints(){ //sorteert de arraylist scoreboard points op basis van punten
+        Collections.sort(scoreboardPoints);
+    }
+
+    @Override
+    public int compareTo(ScoreBoard compareScore){ //zorgt er voor dat het goed gesorteerd word
+        int comparePoints =((ScoreBoard)compareScore).getPoints();
+        return this.points - comparePoints;
+    }
+
     public static ArrayList<ScoreBoard> getScoreboardPoints() {
         return scoreboardPoints;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
     }
 
     public int getPoints() {
@@ -32,5 +71,18 @@ public class ScoreBoard {
 
     public int getUserID() {
         return UserID;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        scoreboardPoints.add(new ScoreBoard(124,1));
+        scoreboardPoints.add(new ScoreBoard(314,2));
+        scoreboardPoints.add(new ScoreBoard(345,3));
+        scoreboardPoints.add(new ScoreBoard(456,4));
+        scoreboardPoints.add(new ScoreBoard(123,5));
+        scoreboardPoints.add(new ScoreBoard(234,6));
+        scoreboardPoints.add(new ScoreBoard(234672834,7));
+        scoreboardPoints.add(new ScoreBoard(234,8));
+        scoreboardPoints.add(new ScoreBoard(543,9));
     }
 }
