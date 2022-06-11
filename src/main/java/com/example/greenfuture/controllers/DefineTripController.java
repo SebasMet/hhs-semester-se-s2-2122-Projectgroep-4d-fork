53a1deab3c0a47d815049cb880aec6ee71e9c4cb
MaikeruB <maikelbazuin@gmail.com>
2022-06-11 18:56:54 +0200

    info button add en gekoppeld met scoreboard

--- src/main/java/com/example/greenfuture/controllers/DefineTripController.java
+++ src/main/java/com/example/greenfuture/controllers/DefineTripController.java
@@ -1,13 +1,16 @@
 package com.example.greenfuture.controllers;
 
 import com.example.greenfuture.PointsAssign;
+import com.example.greenfuture.ScoreBoard;
 import com.example.greenfuture.Trip;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -22,6 +25,12 @@ public class DefineTripController extends Controller {
     public TextField distanceField;
     @FXML
     private Button confirmBtn;
+    @FXML
+    private ListView<String> InfoList;
+    @FXML
+    private Button InfoButton;
+
+
 
     private final ObservableList<String> vehicleList = FXCollections.observableArrayList(
             "Benzine auto", "Hybride auto", "Diesel auto", "Elektrische auto", "Fiets", "Bus"
@@ -49,6 +58,16 @@ public class DefineTripController extends Controller {
         calculatePoints(trip);
     }
 
+    @FXML // zorgt er voor dat als de Info button wordt geclickt de InfoList wordt weergegeven en als de list er is, verdwijnt het.
+    public void InfoButtonCLicked(Event e){
+        if (InfoList.isVisible()){
+            InfoList.setVisible(false);
+        }else{
+            InfoList.setVisible(true);
+        }
+    }
+
+
     private void calculatePoints(Trip trip) {
         PointsAssign pointsAssign = new PointsAssign();
         int points = pointsAssign.calcPoints(trip);
@@ -57,7 +76,9 @@ public class DefineTripController extends Controller {
 
     private void saveTripData(Trip trip, int points) {
         UserRepository users = UserRepository.getInstance();
+        ScoreBoard scoreBoard = new ScoreBoard(1,"!");
         // users geeft issues met de hashmap
+        ScoreBoard.AddPoints(points, trip.getUser());
         users.addPoints(trip.getUser(), points);
         System.out.println(users.getPoints(trip.getUser()));
     }
@@ -65,5 +86,6 @@ public class DefineTripController extends Controller {
     public void initialize() {
         //vehicleField.getItems().addAll(vehicleList);
         vehicleField.setItems(vehicleList);
+        InfoList.getItems().addAll("Punten: ", "Fiets, 0", "Elektrische auto, 3", "Bus, 7", "Hybride auto, 12", "Diesel auto, 18", "Diesel auto, 20");
     }
 }
